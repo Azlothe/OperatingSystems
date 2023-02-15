6fbacdc268fad4d84ef29e8728d19f97d74c5f34
Azlothe <bbqian7@gmail.com>
2023-02-14 20:24:08 -0800

    add file passing

--- A1/countnames.c
+++ A1/countnames.c
@@ -4,6 +4,12 @@
 #define maxNames 100
 #define maxLength 31
 
+
+// TODO print warning messages to stderr
+// check paramters for files
+
+
+
 char names[maxNames][maxLength];
 int count[maxNames];
 int registeredNames = 0;
@@ -17,21 +23,28 @@ int indexOf(char compare[]){
     return -1;
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc <= 1) return 0; // no arguments passed
+
+    FILE *fp = fopen(argv[1], "r");
+    if (fp == NULL){
+        printf("error: cannot open file\n");
+        return 1;
+    }
+
+    char lineInput[maxLength];
+
+    // terminate if passed empty file
+    if (fgets(lineInput, maxLength, fp) == NULL)
+        return 0;
 
     for (int i = 0; i < maxNames; i++)
         for (int j = 0; j < maxLength; j++)
             names[i][j] = '\0';
 
-    char lineInput[maxLength];
-
-    //     printf("%s", names[0]);
-
-    for (int i = 0; i < 7; i++){
-
-        printf("Enter input\n");
-//         scanf("%s", lineInput);
-        fgets(lineInput, maxLength, stdin);
+    do {
+        // skip empty lines
+        if (strcmp(lineInput,"\n") == 0) continue;
 
         lineInput[strlen(lineInput) - 1] = '\0';
 
@@ -50,9 +63,12 @@ int main() {
         else
             ++count[result];
     }
+    while(fgets(lineInput, maxLength, fp) != NULL);
 
     for (int i = 0; i < registeredNames; i++)
         printf("%s: %d\n", names[i], count[i]);
+
+    fclose(fp);
 
     return 0;
 }
