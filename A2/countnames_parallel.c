024bee9fa4db2dcb9d62569b2ccca7f4ec8dec36
Azlothe <bbqian7@gmail.com>
2023-03-02 15:03:28 -0800

    pipe connect

--- A2/countnames_parallel.c
+++ A2/countnames_parallel.c
@@ -11,9 +11,19 @@ typedef struct name {
     int count;
 } name;
 
-name nameCount[maxNames] = { {'\0',0} };
+const name empty = {'\0', 0};
 
+name nameCount[maxNames];
 int registeredNames = 0;
+
+const long bufferSize = maxNames * sizeof(name);
+
+int status;
+
+// equality will be the name.input
+int equals(name o1, name o2) {
+    return (strcmp(o1.input, o2.input) == 0);
+}
 
 
 /**
@@ -32,10 +42,16 @@ int indexOf(char compare[]){
     return -1;
 }
 
-int main(int argc, char *argv[]) {
+name find(name n) {
+    for (int i = 0; i < registeredNames; i++)
+        if (equals(n, nameCount[i]))
+            return nameCount[i];
 
-    // TODO
-    // check if any arguments passed with argc
+    //     return NULL;
+    return empty;
+}
+
+int main(int argc, char *argv[]) {
 
     if (argc < 2) {
         printf("No input file given\n");
@@ -45,6 +61,9 @@ int main(int argc, char *argv[]) {
     int num = 0; // for parent, num = number of child processes; for child, num = their index in child[] and an assigned text file from argv[]
     pid_t childPID; // to determine if is spawned child
     pid_t child[argc - 1]; // store PID of child process
+
+    int fd[2];
+    pipe(fd); // all children will share pipe
 
     while (num < argc - 1 && (childPID = fork()) > 0)
         child[num++] = childPID;
@@ -56,24 +75,28 @@ int main(int argc, char *argv[]) {
         FILE *fp = fopen(fileName, "r");
         if (fp == NULL){
             printf("range: cannot open file %s\n", fileName);
-            return 1; // TODO check this at parent wait(NULL) and dont read anything
+            status = -1;
+            write(fd[1], &status, sizeof(int));
+
+            return 1;
         }
 
         char lineInput[maxLength];
 
         // terminate if passed empty file
         if (fgets(lineInput, maxLength, fp) == NULL) {
-            fprintf(stderr, "File %s is empty", fileName);
-            return 0; // TODO check this at parent wait(NULL) and dont read anything
+            fprintf(stderr, "File %s is empty\n", fileName);
+            status = -1;
+            write(fd[1], &status, sizeof(int));
+
+            return 0;
         }
+
+        close(fd[0]); // close child read
 
         // initialize name array with null characters
-        for (int i = 0; i < maxNames; i++) {
-            for (int j = 0; j < maxLength; j++)
-                nameCount[i].input[j] = '\0';
-
-            nameCount[i].count = 0;
-        }
+        for (int i = 0; i < maxNames; i++)
+            nameCount[i] = empty;
 
         int lineCounter = 0;
 
@@ -89,14 +112,11 @@ int main(int argc, char *argv[]) {
             // remove the newline character from fgets
             lineInput[strlen(lineInput) - 1] = '\0';
 
-
             int result = indexOf(lineInput);
-
 
             if (result <= -1) {
 
                 // new input is not registered; register it
-
                 name newName;
 
                 strcpy(newName.input,lineInput);
@@ -111,18 +131,54 @@ int main(int argc, char *argv[]) {
         }
         while(fgets(lineInput, maxLength, fp) != NULL); // EOF
 
-        for (int i = 0; i < registeredNames; i++)
-            printf("%s: %d\n", nameCount[i].input, nameCount[i].count);
+        write(fd[1], &registeredNames, sizeof(int));
+        write(fd[1], nameCount, bufferSize);
     }
     else if (childPID > 0) {
 
-        for (int i = 0; i < num; i++) {
-            //             waitpid(child[i], NULL, 0);
+        close(fd[1]); // close parent write
+
+
+        // first child process to end; use it as a base counter
+        wait(NULL);
+
+        read(fd[0], &status, sizeof(int));
+
+        if(status > -1) {
+            registeredNames = status;
+            read(fd[0], nameCount, bufferSize);
+        }
+
+        int childSize;
+        name childName[maxNames];
+
+
+        // for the rest of the children
+        for (int i = 1; i < num; i++) {
             wait(NULL);
 
+            read(fd[0], &status, sizeof(int));
 
-            printf("\nran %d\n",i);
+            if(status <= -1)
+                continue;
 
+            read(fd[0], childName, bufferSize);
+
+            int j = 0;
+            int result;
+            name current;
+
+            while(j < maxNames && !equals(childName[j], empty)){
+
+                current = find(childName[j]);
+
+                if (equals(current, empty))
+                    nameCount[registeredNames++] = childName[j];
+                else
+                    current.count += childName[j].count;
+
+                ++j;
+            }
         }
 
         // print names and their corresponding count
